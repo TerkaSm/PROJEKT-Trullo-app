402521d0f4421669d0be420cebef6ad7df6e22d6
Tereza Smolová <framboiseart@email.cz>
2024-01-16 14:37:56 +0100

    working in progress

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,15 +1,23 @@
 import { List } from "../../components/List";
 import { AddForm } from "../../components/AddForm";
+import { data } from "../../../data";
 
 export const HomePage = () => {
+
+  console.log(data)
   return (
     <div className="container">
       <main className="flex min-h-screen w-screen bg-yellow-300">
         <div className="sm:flex items-start w-screen p-10 overflow-x-auto">
-          <List title="Dnešní plán" />
+          {data.map(list => {
+            console.log(`ID: ${list.id}`);
+            console.log(`Title: ${list.title}`);
+            <List title={list.title} key={list.id}  cards={list.cards} />
+          })}
           <AddForm />
         </div>
       </main>
     </div>
   );
 };
+
